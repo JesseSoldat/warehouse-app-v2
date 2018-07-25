3ef4558bf972267e2f6c1e79c4aaa7c4875824da
Jesse <jsoldat@hotmail.com>
2018-07-25 14:13:19 +0700

    can seed boxes

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,17 +2,19 @@ const seedProducts = require("./seeding/productSeed");
 const seedProducer = require("./seeding/producerSeed");
 const seedCustomer = require("./seeding/customerSeed");
 const storageSeed = require("./seeding/storageSeed");
+const boxSeed = require("./seeding/boxSeed");
 
 const seedDb = async () => {
   try {
     // await seedProducts();
     // await seedProducer();
     // await seedCustomer();
-    await storageSeed();
+    // await storageSeed();
+    await boxSeed();
   } catch (err) {
     console.log("Error while seeding the DB.");
   }
 };
 
 // Uncomment to seed db
-// seedDb();
+seedDb();
